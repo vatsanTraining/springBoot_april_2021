573eb056006c71fc6f73bea1f7d6980000382f8c
srivatsan <vatsank@gmail.com>
2021-04-10 16:59:41 +0530

    Update

--- invoice-service/src/main/java/com/example/demo/InvoiceServiceApplication.java
+++ invoice-service/src/main/java/com/example/demo/InvoiceServiceApplication.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.Invoice;
+import com.example.demo.repos.InvoiceRepository;
 
 @SpringBootApplication
 public class InvoiceServiceApplication {
@@ -10,4 +18,21 @@ public class InvoiceServiceApplication {
 		SpringApplication.run(InvoiceServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			InvoiceRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+		
+				repo.save(new Invoice(101,"Ramesh",LocalDate.of(2021, 4, 8),6000));
+				repo.save(new Invoice(102,"Suresh",LocalDate.of(2021, 3, 28),7000));
+				
+			}
+		};
+	}
 }
