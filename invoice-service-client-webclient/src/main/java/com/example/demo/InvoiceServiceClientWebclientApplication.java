0dc3d527f3a787922b611232da9397aec9a02962
srivatsan <vatsank@gmail.com>
2021-04-11 12:51:05 +0530

    Reactive WebClient Example

---
+++ invoice-service-client-webclient/src/main/java/com/example/demo/InvoiceServiceClientWebclientApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class InvoiceServiceClientWebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceServiceClientWebclientApplication.class, args);
+	}
+
+	@Bean
+	public WebClient.Builder builder(){
+		
+		return WebClient.builder();
+	}
+	
+
+	
+	
+	@Bean
+	public WebClient client(WebClient.Builder builder) {
+		return builder.build();
+	}
+
+}
