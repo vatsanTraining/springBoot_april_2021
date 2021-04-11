aaaddaed9ee6a839aa4888718253ee82812d7eb8
srivatsan <vatsank@gmail.com>
2021-04-11 11:07:21 +0530

    DSL Methods added to Repository

--- invoice-service/src/main/java/com/example/demo/repos/InvoiceRepository.java
+++ invoice-service/src/main/java/com/example/demo/repos/InvoiceRepository.java
@@ -1,11 +1,23 @@
 package com.example.demo.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Invoice;
+import java.util.*;
 
+import javax.transaction.Transactional;
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 
+	
+	public List<Invoice> findBycustomerName(String srchName);
+	
+	
+	@Query(value = "update Invoice set amount = amount + amount*.10")
+	@Modifying
+	@Transactional
+	public int updateAmount();
 }
