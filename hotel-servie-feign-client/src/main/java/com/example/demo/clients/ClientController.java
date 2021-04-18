be6d93d87ed8a3f3c588dca261920e323d2938b6
srivatsan <vatsank@gmail.com>
2021-04-18 16:59:50 +0530

    Cloud Gateway Added

--- hotel-servie-feign-client/src/main/java/com/example/demo/clients/ClientController.java
+++ hotel-servie-feign-client/src/main/java/com/example/demo/clients/ClientController.java
@@ -1,6 +1,5 @@
 package com.example.demo.clients;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
