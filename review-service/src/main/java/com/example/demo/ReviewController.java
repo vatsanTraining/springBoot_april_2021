4812a32ad7fdafa154fb4169bf16220c03431691
srivatsan <vatsank@gmail.com>
2021-04-18 10:52:10 +0530

    Clustered Registry Client

---
+++ review-service/src/main/java/com/example/demo/ReviewController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+
+	
+	@GetMapping(path = "/reviews")
+	public String getReviews() {
+		
+		return "Average rating 4.5 out of 1230";
+	}
+}
