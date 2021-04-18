6b7cf15f7c52ae744fa61ad6ed5ff03b0ba85453
srivatsan <vatsank@gmail.com>
2021-04-18 16:37:57 +0530

    FeignClient Example Added

---
+++ hotel-servie-feign-client/src/main/java/com/example/demo/clients/CleintController.java
@@ -0,0 +1,21 @@
+package com.example.demo.clients;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CleintController {
+
+	
+	@Autowired
+	private HotelServiceClient service;
+	
+	@GetMapping(path = "/client/hotels")
+	public List<String> getAll() {
+		
+		return this.service.getAllHotels();
+	}
+}
---
+++ hotel-servie-feign-client/src/main/java/com/example/demo/clients/HotelServiceClient.java
@@ -0,0 +1,13 @@
+package com.example.demo.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+@FeignClient(name = "HOTEL-SERVICE")
+public interface HotelServiceClient {
+
+	
+	@GetMapping(path = "/api/v1/hotels")
+	public List<String> getAllHotels();
+}
