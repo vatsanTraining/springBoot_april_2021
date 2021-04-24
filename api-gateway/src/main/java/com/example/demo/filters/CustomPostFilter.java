bed94ab07e0749fda6175681b0a846f20f9b108c
srivatsan <vatsank@gmail.com>
2021-04-24 12:12:06 +0530

    Update

--- api-gateway/src/main/java/com/example/demo/filters/CustomPostFilter.java
+++ api-gateway/src/main/java/com/example/demo/filters/CustomPostFilter.java
@@ -3,9 +3,11 @@ package com.example.demo.filters;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 
 import reactor.core.publisher.Mono;
 
+@Component
 public class CustomPostFilter extends AbstractGatewayFilterFactory<CustomPostFilter.Config> {
 
 public static class Config{
