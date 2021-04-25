9684a7ad9122aa73a63908ecceb51db42b681b53
srivatsan <vatsank@gmail.com>
2021-04-25 12:39:47 +0530

    Update PKCE example

---
+++ oauth2-pkce/src/main/java/com/example/demo/Oauth2PkceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PkceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2PkceApplication.class, args);
+	}
+
+}
---
+++ oauth2-pkce/src/main/java/com/example/demo/controllers/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+		  .antMatchers("/","/actuator")
+		    .permitAll().antMatchers("/reviews").authenticated().and().oauth2Login();
+		  
+	
+	
+	}
+
+	
+}
