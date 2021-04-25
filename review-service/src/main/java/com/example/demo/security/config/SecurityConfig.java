8701d4bcb2efca62ba7d8a68ee2e07c3e6939467
srivatsan <vatsank@gmail.com>
2021-04-25 09:48:15 +0530

    Update

--- review-service/src/main/java/com/example/demo/security/config/SecurityConfig.java
+++ review-service/src/main/java/com/example/demo/security/config/SecurityConfig.java
@@ -8,14 +8,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+
+		 auth.inMemoryAuthentication()
+		    .withUser("india")
+		      .password("{noop}india")
+		        .and()
+		         .withUser("GUEST");
+		 
+	
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+
+		http.authorizeRequests()
+		       .antMatchers("/reviews")
+		           .authenticated()
+		                .and()
+		                  .httpBasic();
 	}
 
 }
