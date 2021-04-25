4216841bc0dd0c52bbb5103811b6f9f3cc0d6708
srivatsan <vatsank@gmail.com>
2021-04-25 13:05:15 +0530

    Update

--- oauth2-pkce/src/main/java/com/example/demo/controllers/ReviewController.java
+++ oauth2-pkce/src/main/java/com/example/demo/controllers/ReviewController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,10 @@ public class ReviewController {
 	    }
 
 	@GetMapping(path = "/reviews")
-	public String getReviews() {
+	public String getReviews(Principal principal) {
 		
+		System.out.println(principal.getName());
+		System.out.println(principal.toString());
 		return "Average rating 4.5 out of 1230";
 	}
 }
